55c84fa5ec6063b296a849604310f759ef9e4569
AhmedSalahMohamedKheir <ahmedkheir.10@gmail.com>
2024-10-31 16:25:35 -0400

    second commit

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,8 +1,12 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("this is the project version 1");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("this is the project version 2");
     }
 }
-//done with version 1
+//done with version 2
 
